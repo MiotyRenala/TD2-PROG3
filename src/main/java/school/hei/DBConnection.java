b51de38cfc06d5508d10f813962de996d0ec2d16
mioty <hei.mioty.2@gmail.com>
2025-12-26 10:15:58 +0300

    Fix : rename value in dbConnection

--- src/main/java/school/hei/DBConnection.java
+++ src/main/java/school/hei/DBConnection.java
@@ -5,15 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private String url = "jdbc:postgresql://localhost:5432/mini_dish_db";
-    private String user = "mini_dish_db_manager";
-    private String password = "123456";
+    private String JDBC_URL = "jdbc:postgresql://localhost:5432/mini_dish_db";
+    private String USERNAME = "mini_dish_db_manager";
+    private String PASSWORD = "123456";
 
     public DBConnection() {
     }
 
     public Connection getDBConnection () throws SQLException {
-        return DriverManager.getConnection(url, user, password);
+        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
     }
 
 }
